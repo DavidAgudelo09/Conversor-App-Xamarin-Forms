07a19d6543a6e5927f83d77ad83de2bbe2ac832a
DavidAgudelo09 <davidagudelo0926@gmail.com>
2023-03-03 22:01:53 -0500

    Agregar archivos de proyecto.

---
+++ Conversor/Vistas/Convertir.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Vistas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Convertir : ContentPage
+	{
+		double cm;
+		double m;
+		public Convertir ()
+		{
+			InitializeComponent ();
+		}
+		private void Calcular()
+		{
+			cm= Convert.ToDouble(txtcm.Text);
+			m = cm / 100;
+			lblresultado.Text = m.ToString() + " m";
+		}
+		private void Validar()
+		{
+			if(!string.IsNullOrEmpty(txtcm.Text))
+			{
+				Calcular();
+			}
+			else
+			{
+				DisplayAlert("Error", "Ingrese una cantidad", "OK");
+			}
+		}
+        private void btnvolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void btncalcular_Clicked(object sender, EventArgs e)
+        {
+			Validar();
+        }
+    }
+}
---
+++ Conversor/Vistas/Menuprincipal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menuprincipal : ContentPage
+    {
+        public Menuprincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Convertir());
+        }
+    }
+}
